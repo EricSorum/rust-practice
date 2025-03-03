3455d6db1d54cc09e67aa16d106d156182b6f1c6
Eric Sorum <ejsorum@gmail.com>
2025-03-03 08:30:50 -0600

    Added loops.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
 fn main() {
     println!("Hello, world!");
-
+    // second_function();
+    // params(5, 'h');
+    looping();
+    for_loop();
 }
 
-fn second_function() {
-  let x = "Some string";
-  println!("{}", x);
+// fn second_function() {
+//   let x = "Some string";
+//   println!("{}", x);
+// }
+
+// fn params(value: u32, unit_label: char) {
+//   println!("The measurement is: {value}{unit_label}");
+// }
+
+fn looping() {
+    let mut num = 3;
+    while num != 0 {
+      println!("{num}!");
+      num -= 1;
+    }
+
+    println!("LIFTOFF!");
+}
+
+fn for_loop() {
+  let arr = [10, 20, 30, 40, 50];
+  let mut i = 0;
+
+  while i < 5 {
+    println!("The value is: {}", arr[i]);
+
+    i += 1;
+  }
 }
